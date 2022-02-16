a3559a66de209c4b0450feae064e474e1e1c37bb
Matthias Wünsch <matthias.wuensch175@gmail.com>
2022-02-16 14:26:19 +0100

    improve filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 // dependencies
 import { Telegraf, Markup } from 'telegraf';
 import { config } from 'dotenv';
@@ -7,7 +5,6 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { rejects } from 'assert';
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
 
 // load environment variables
 config();
@@ -34,7 +31,7 @@ let hrtDate = new Date(process.env.HRT_DATE)
 let untilHrt = toDays(hrtDate - getNow())
 let reg = "\${X}"
 
-async function queryGPT3(data="euphoria") {
+async function queryGPT3(data = "euphoria") {
     let authorization = process.env.OPENAI_TOKEN;
     let url = "https://api.openai.com/v1/engines/davinci-codex/completions";
 
@@ -73,20 +70,35 @@ async function queryGPT3(data="euphoria") {
 
 // #region
 bot.command('hrt', (ctx) => {
-    queryGPT3("hrt-sentences")
-    .then(data => {
-        console.log(getNow())
-        response = data.choices[0].text.replace(/[(\$*)]/g, untilHrt)
-        unsafeHRTWords = []
-        if (response.indexOf(unsafeHRTWords) == -1){
-            ctx.reply(response)    
-        }
-        else {
-            ctx.reply("unsafe word")
-        }
+    getHRT().then(response => {
+        ctx.reply(response)
+    })
+    .catch(err => {
+        ctx.reply("1: an error occured")
+    })
+})
+
+function getHRT() {
+    return new Promise((resolve) => {
+        queryGPT3("hrt-sentences")
+            .then(data => {
+                console.log(getNow())
+                let response = data.choices[0].text.replace(/[(\$*)]/g, untilHrt)
+                let unsafeHRTWords = ["woman", "women", "suicide", "death", "pain", "titties", "tits"]
+                if (unsafeHRTWords.indexOf(response) === -1) {
+                    resolve(response)
+                }
+                else {
+                    if (ErrCounter < 5) {
+                        resolve(getHRT())
+                    } else {
+                        reject("unsafe word")
+                    }
+                }
+            })
     })
 
-})
+}
 
 
 //#endregion
